69acbaefe2f9b2040413d65d5cf1fc5951332ce0
Ross bertrand <rossbertrand@ROSS-M1-MBP.local>
2025-12-01 16:38:29 -0500

    Minimal loader - icon only, no text

--- src/components/Shared/SpartanLoader.js
+++ src/components/Shared/SpartanLoader.js
@@ -1,42 +1,16 @@
-// src/components/Shared/SpartanLoader.js - FIXED to use helmet.png properly
+// src/components/Shared/SpartanLoader.js
 import React from 'react';
 import './SpartanLoader.css';
-import helmetImage from '../../assets/helmet.png';
 
-const SpartanLoader = ({ 
-  size = 50, // UPDATED: Changed from 80 to 50 for smaller default size
-  message = "Loading...", 
-  showMessage = true,
-  animationType = "default" // default, intense, breathing, warrior
-}) => {
-  
-  // Choose animation class based on type
-  const getAnimationClass = () => {
-    switch(animationType) {
-      case "intense":
-        return "spartan-helmet-intense";
-      case "breathing": 
-        return "spartan-helmet-breathing";
-      case "warrior":
-        return "spartan-helmet-warrior";
-      default:
-        return "spartan-helmet-image";
-    }
-  };
-
+const SpartanLoader = ({ size = 48 }) => {
   return (
     <div className="spartan-loader-container">
-      <div className="spartan-loader-animation">
-        <img 
-          src={helmetImage}
-          alt="Loading" 
-          className={getAnimationClass()}
-          style={{ width: size, height: size }}
-        />
-      </div>
-      {showMessage && (
-        <p className="spartan-loader-message">{message}</p>
-      )}
+      <img 
+        src="/icon-192.png"
+        alt="" 
+        className="spartan-loader-icon"
+        style={{ width: size, height: size }}
+      />
     </div>
   );
 };
